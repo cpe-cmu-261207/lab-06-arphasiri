2bda921687dc762bb9d31fb9c29fae5939340881
arphasiri <arpasiri6151@gmail.com>
2021-08-29 23:43:21 +0700

    edit name

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className='text-center space-y-3'>
             <p className='text-2xl font-semibold'>About me</p>
-            <p className='text-xl'>Arpasiri Khongngern 630610771</p>
+            <p className='text-xl'>Arpasiri Kongngern 630610771</p>
         </div>
     )
 }
