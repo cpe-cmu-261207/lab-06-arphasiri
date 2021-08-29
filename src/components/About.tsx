af04a73144f5e3735ae93aa869a5cd1d238e5285
arphasiri <arpasiri6151@gmail.com>
2021-08-29 23:35:04 +0700

    5555

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Arpasiri Khongngern 630610771</p>
+        </div>
+    )
+}
+
+export default About
